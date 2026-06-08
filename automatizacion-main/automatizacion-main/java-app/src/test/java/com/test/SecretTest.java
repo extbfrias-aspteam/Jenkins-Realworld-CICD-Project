a19edbdf6ddfc9a51cd222f1e50beadcfb3845fb
Benjamin Juarez <extbfrias@aspintegraopciones.com>
2026-06-08 03:00:18 -0600

    change secrets

--- automatizacion-main/automatizacion-main/java-app/src/test/java/com/test/SecretTest.java
+++ automatizacion-main/automatizacion-main/java-app/src/test/java/com/test/SecretTest.java
@@ -2,12 +2,11 @@ package com.test;
 
 public class SecretTest {
     // 1. Simular un Token de AWS (Usa el prefijo oficial AKIA que busca SonarQube)
-    private static final String AWS_KEY = "AKIAIOSFODNN7EXAMPLE"; 
-    private static final String AWS_SECRET = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
-
+    private static final String AWS_KEY = ""; 
+    private static final String AWS_SECRET = ""
     // 2. Asignación explícita con alta entropía (texto largo y complejo)
     public void connectDatabase() {
-        String db_password = "g8X#mK2!pL9QzWvR4tY7uE1iO0pA3sD6"; // Contraseña compleja hardcodeada
-        String apiKey = "ghp_vR4tY7uE1iO0pA3sD6mK2pL9QzWvR4tY7uE1"; // Patrón de Token de GitHub
+        String db_password = ""; // Contraseña compleja hardcodeada
+        String apiKey = ""; // Patrón de Token de GitHub
     }
 }
